64e82f102d7b22bca28edb20d98a5c38bf50de00
Kristian Heljas <kristian@kristian.ee>
2021-03-21 15:40:00 +0200

    ci: configure automatic semantic-release

---
+++ release.config.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-template-curly-in-string */
+module.exports = {
+  branches: ['main', { name: 'dev', prerelease: 'dev' }],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'docs/CHANGELOG.md',
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'docs/CHANGELOG.md'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+    ['@semantic-release/github', { assets: 'node-red-contrib-z2m-*.tgz' }],
+    '@semantic-release/npm',
+  ],
+};
