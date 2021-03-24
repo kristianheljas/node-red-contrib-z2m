7956b5bcc819f1de302515ff47c423bfaa0288a0
Kristian Heljas <kristian@kristian.ee>
2021-03-24 08:44:51 +0200

    feat(api): added basic broker information and device list

--- src/api/index.ts
+++ src/api/index.ts
@@ -30,8 +30,49 @@ export class Z2mApi {
     this.red.httpAdmin.use(`/${PACKAGE_NAME}`, this.router);
   }
 
+  private setupRoutes() {
+    this.router.use('/assets', serveStaticAssets);
+
+    this.router.get('/info', (_req, res) => {
+      res.json(this.info());
+    });
+
+    this.router.get('/broker/:brokerId', (req, res) => {
+      res.send(this.brokerInfo(req.params.brokerId));
+    });
+
+    this.router.get('/broker/:brokerId/devices', (req, res) => {
+      res.json(this.brokerDevices(req.params.brokerId));
+    });
+  }
+
   private info() {
     return { ...this.packageInfo, brokers: this.brokers() };
+  }
+
+  private brokerInfo(brokerId: string) {
+    const broker = this.red.nodes.getNode(brokerId);
+
+    if (broker instanceof Z2mBrokerNode) {
+      const { state, bridge } = broker;
+      return { state, bridge };
+    }
+
+    throw new Error(
+      `Node ID '${brokerId}' does not refer to a '${Z2mBrokerNode.type}' node, got '${broker.type}' instead!`,
+    );
+  }
+
+  private brokerDevices(brokerId: string) {
+    const broker = this.red.nodes.getNode(brokerId);
+
+    if (broker instanceof Z2mBrokerNode) {
+      return broker.devices;
+    }
+
+    throw new Error(
+      `Node ID '${brokerId}' does not refer to a '${Z2mBrokerNode.type}' node, got '${broker.type}' instead!`,
+    );
   }
 
   private brokers(): Z2mBrokerNodeDef[] {
@@ -42,14 +83,6 @@ export class Z2mApi {
       }
     });
     return brokers;
-  }
-
-  private setupRoutes() {
-    this.router.use('/assets', serveStaticAssets);
-
-    this.router.get('/info', (_req, res) => {
-      res.json(this.info());
-    });
   }
 
   static register(RED: NodeAPI): Z2mApi {
