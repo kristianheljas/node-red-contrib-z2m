dea918ad5dddd0b400f1e12ab69f5beae64ff01c
Kristian Heljas <kristian@kristian.ee>
2021-03-24 15:13:51 +0200

    chore: add property and changed options to device input node

--- src/nodes/device/in.ts
+++ src/nodes/device/in.ts
@@ -5,11 +5,13 @@ import { Z2mNode, CheckNodeOptions, Z2mNodeDef } from '../../core/z2m-node';
 
 interface Z2mDeviceOutNodeDef extends Z2mNodeDef {
   topic: string;
-  initial: boolean;
+  property: string;
+  onlyWhenChanged: boolean;
+  sendInitialValue: boolean;
 }
 
 @CheckNodeOptions
-class Z2mDeviceOutNode extends Z2mNode<Z2mDeviceOutNodeDef> {
+class Z2mDeviceInNode extends Z2mNode<Z2mDeviceOutNodeDef> {
   static type = 'z2m-device-in';
 
   lastState: Z2mDeviceState = {};
@@ -18,11 +20,31 @@ class Z2mDeviceOutNode extends Z2mNode<Z2mDeviceOutNodeDef> {
     this.z2m.subscribe(this.config.topic, 0, this.onStateMessage.bind(this), this);
   }
 
+  preparePayload(state: Z2mDeviceState) {
+    if (this.config.property) {
+      return state[this.config.property] ?? null;
+    }
+    return state;
+  }
+
   onStateMessage(topic: string, buffer: Buffer, { retain, qos }: IPublishPacket) {
     const state = JSON.parse(buffer.toString()) as Z2mDeviceState;
 
-    if (retain === false || this.config.initial === true) {
-      this.send({ topic, retain, qos, state, payload: state });
+    const lastPayload = this.preparePayload(this.lastState);
+    const payload = this.preparePayload(state);
+
+    let shouldSend = true;
+
+    if (this.config.onlyWhenChanged && payload === lastPayload) {
+      shouldSend = false;
+    }
+
+    if (this.config.sendInitialValue === false && retain === true) {
+      shouldSend = false;
+    }
+
+    if (shouldSend) {
+      this.send({ topic, retain, qos, state, payload });
     }
 
     this.lastState = state;
@@ -30,5 +52,5 @@ class Z2mDeviceOutNode extends Z2mNode<Z2mDeviceOutNodeDef> {
 }
 
 export = (RED: NodeAPI): void => {
-  Z2mDeviceOutNode.register(RED);
+  Z2mDeviceInNode.register(RED);
 };
