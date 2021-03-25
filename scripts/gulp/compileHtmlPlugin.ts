60141370dd194d412665ce73726eafcc0a876043
Kristian Heljas <kristian@kristian.ee>
2021-03-25 16:31:07 +0200

    refactor: separate edit and help templates

--- scripts/gulp/compileHtmlPlugin.ts
+++ scripts/gulp/compileHtmlPlugin.ts
@@ -1,12 +1,11 @@
 /* eslint-disable no-param-reassign */
-import { Transform } from 'stream';
-import Vinyl from 'vinyl';
 import ejs from 'ejs';
-import { join } from 'path';
-import through, { TransformFunction } from 'through2';
-
-import PluginError from 'plugin-error';
 import fs from 'fs';
+import { join } from 'path';
+import PluginError from 'plugin-error';
+import { Transform } from 'stream';
+import through, { TransformFunction } from 'through2';
+import Vinyl from 'vinyl';
 
 const { stat, readFile } = fs.promises;
 
@@ -21,8 +20,6 @@ const transform: TransformFunction = async (file: Vinyl, encoding, callback) => 
     return;
   }
 
-  const relativeTsPath = file.relative;
-
   file.extname = '.html';
 
   // If html file already exists, skip everything else
@@ -35,19 +32,28 @@ const transform: TransformFunction = async (file: Vinyl, encoding, callback) => 
   const { cwd, base, dirname, stem } = file;
   const viewsPath = join(cwd, base, 'views');
 
+  const nodePath = join(dirname, 'node.ts');
+  const node = require(nodePath).default; // eslint-disable-line
+
   const templatePath = join(dirname, `${stem}.ejs`);
-  const templateData: ejs.Data = {};
+  const templateData: ejs.Data = { node };
   const templateOptions: ejs.Options = {
-    views: [viewsPath],
+    views: [dirname, viewsPath],
   };
 
-  if (await exists(templatePath)) {
-    file.contents = await ejs.renderFile(templatePath, templateData, templateOptions).then(Buffer.from);
-    callback(null, file);
-    return;
-  }
+  try {
+    if (await exists(templatePath)) {
+      file.contents = await ejs.renderFile(templatePath, templateData, templateOptions).then(Buffer.from);
+      callback(null, file);
+      return;
+    }
 
-  callback(new PluginError(`No suitable templates found for '${relativeTsPath}'`, __filename));
+    const defaultTemplate = join(viewsPath, 'default-index.ejs');
+    file.contents = await ejs.renderFile(defaultTemplate, templateData, templateOptions).then(Buffer.from);
+    callback(null, file);
+  } catch (err) {
+    callback(new PluginError('Something went wrong when compiling template!', err));
+  }
 };
 
 export default (): Transform => through.obj(transform);
