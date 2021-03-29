55fa57a2a451ad50f33edb35651ff26791b5cabf
Kristian Heljas <kristian@kristian.ee>
2021-03-30 00:19:08 +0300

    chore: always return json from api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,4 @@
-import express, { Router } from 'express';
+import express, { ErrorRequestHandler, Router } from 'express';
 import type { NodeAPI } from 'node-red';
 import { PACKAGE_NAME, PACKAGE_NODES, PACKAGE_VERSION } from '../core/constants';
 import Z2mBrokerNode, { Z2mBrokerNodeDef } from '../nodes/broker/node';
@@ -29,23 +29,44 @@ export class Z2mApi {
       brokers: [],
     };
 
-    this.router = Router();
-    this.setupRoutes();
+    this.router = this.setupRouter();
     this.red.httpAdmin.use(`/${PACKAGE_NAME}`, this.router);
   }
 
-  private setupRoutes() {
-    this.router.use('/editor', serveEditorAssets);
+  private setupRouter() {
+    const router = Router();
 
-    this.router.get('/info', (_req, res) => {
+    router.use('/editor', serveEditorAssets);
+
+    this.registerRoutes(router);
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const errorHandler: ErrorRequestHandler = (error, req, res, next) => {
+      res.status(500).json({
+        message: error instanceof Error ? error.message : error,
+      });
+    };
+    router.use(errorHandler);
+
+    router.get('*', (req, res) => {
+      res.status(404).json({
+        message: `404 Not Found (${req.path})`,
+      });
+    });
+
+    return router;
+  }
+
+  private registerRoutes(router: Router) {
+    router.get('/info', (_req, res) => {
       res.json(this.info());
     });
 
-    this.router.get('/broker/:brokerId', (req, res) => {
+    router.get('/broker/:brokerId', (req, res) => {
       res.send(this.brokerInfo(req.params.brokerId));
     });
 
-    this.router.get('/broker/:brokerId/devices', (req, res) => {
+    router.get('/broker/:brokerId/devices', (req, res) => {
       res.json(this.brokerDevices(req.params.brokerId));
     });
   }
@@ -62,11 +83,7 @@ export class Z2mApi {
       return { state, bridge };
     }
 
-    throw new Error(
-      `Node ID '${brokerId}' does not refer to a '${Z2mBrokerNode.type}' node, got '${
-        broker?.type ?? 'undefined'
-      }' instead!`,
-    );
+    throw new Error(`Broker not found!`);
   }
 
   private brokerDevices(brokerId: string) {
@@ -76,11 +93,7 @@ export class Z2mApi {
       return broker.devices;
     }
 
-    throw new Error(
-      `Node ID '${brokerId}' does not refer to a '${Z2mBrokerNode.type}' node, got '${
-        broker?.type ?? 'undefined'
-      }' instead!`,
-    );
+    throw new Error(`Broker not found!`);
   }
 
   private brokers(): Z2mBrokerNodeDef[] {
